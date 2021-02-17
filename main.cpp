ab72f892f6991120f8c0e68e24c775d0ea8bd595
pereiradaniel <danielpaulpereira@protonmail.com>
2021-02-17 13:22:20 -0500

    Use list and iterator.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <list>
 
 using namespace std;
 
 void display(int arr[], size_t size) {
-	for (auto i = 0; i < size; ++i) {
+	for (auto i = 0u; i < size; ++i) {
 		cout << arr[i] << ", ";
 	}
 }
 
 void display(std::array<int, 5> arr) {
-	for (auto i = 0; i < arr.size(); ++i) {
+	for (auto i = 0u; i < arr.size(); ++i) {
 		cout << arr[i] << ", ";
 	}
 }
@@ -20,24 +21,28 @@ int main()
 {
 	// advantage: fast retrieval of the element at a random index 'i'
 	// disadvantage: cannot be resized or very expensive to resize
-	int arr_1[]{ 5, 8, 32, 9, 1 };
+	/*int arr_1[]{ 5, 8, 32, 9, 1 };
 	
-	int arr_2[5];
+	int arr_2[5];*/
 
-	// STL: Standard Template Libraru
-	std::array<int, 5> arrSTL_1{};
-	std::array<int, 5> arrSTL_2{};
+	
 
-	std::vector<int> coll; // dynamically allocated array.
+	// std::vector<int> coll; // dynamically allocated array.
+	std::list<int> coll;
 	coll.push_back(5);
 	coll.push_back(8);
 	coll.push_back(32);
 	coll.push_back(9);
 	coll.push_back(1);
 
-	for (auto i = 0; i < coll.size(); ++i)
-		cout << coll[i] << ", ";
+	std::list<int>::iterator it = coll.begin();
+	for (; it != coll.end(); ++it)
+		cout << *it << ", "; // type *it is an integer
 	cout << endl;
+
+	//for (auto i = 0; i < coll.size(); ++i)
+	//	cout << coll[i] << ", ";
+	//cout << endl;
 	
 	for (auto& item : coll)
 		cout << item << ", ";
